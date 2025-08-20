5a48d9a4fe6cfe139e7abc9e9065ad00d5dc2f0d
Daniel-Lidman <dlidman@gmail.com>
2025-08-20 17:36:15 -0500

    git init

---
+++ HackingWithSwift.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let sports = ["Baseball", "Basketball", "Soccer", "Football", "Cricket", "Hockey", "Baseball", "Basketball"]
+let uniqueSports = Set(sports)
+print("Number of sports: \(sports.count)")
+print("Number of unique sports: \(uniqueSports.count)")
