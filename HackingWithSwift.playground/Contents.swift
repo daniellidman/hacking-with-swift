a180f43f69de92bec1689ddd3b2865159f1e376a
Daniel-Lidman <dlidman@gmail.com>
2025-08-21 13:03:15 -0500

    source projects

--- HackingWithSwift.playground/Contents.swift
+++ HackingWithSwift.playground/Contents.swift
@@ -4,3 +4,12 @@ let sports = ["Baseball", "Basketball", "Soccer", "Football", "Cricket", "Hockey
 let uniqueSports = Set(sports)
 print("Number of sports: \(sports.count)")
 print("Number of unique sports: \(uniqueSports.count)")
+
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let password = "HELLO, WORLD"
+let result = isUppercase(password)
+print(result)
