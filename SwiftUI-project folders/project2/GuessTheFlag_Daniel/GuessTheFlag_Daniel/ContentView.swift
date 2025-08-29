c50b8a1e637071518f7659bb75095aa7d20bfe74
Daniel-Lidman <dlidman@gmail.com>
2025-08-28 21:12:47 -0500

    Day 23

--- SwiftUI-project folders/project2/GuessTheFlag_Daniel/GuessTheFlag_Daniel/ContentView.swift
+++ SwiftUI-project folders/project2/GuessTheFlag_Daniel/GuessTheFlag_Daniel/ContentView.swift
@@ -8,31 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var countries = [
+        "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland",
+        "Spain", "UK", "Ukraine", "US",
+    ].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    
+
     @State private var showingScore = false
     @State private var showingFinalScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
     @State private var scoreMessage = ""
     @State private var questionNumber = 0
-    
+
     var body: some View {
-        ZStack{
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.2), location: 0.3)
-            ], center: .top, startRadius: 200, endRadius: 700)
+        ZStack {
+            RadialGradient(
+                stops: [
+                    .init(
+                        color: Color(red: 0.1, green: 0.2, blue: 0.45),
+                        location: 0.3
+                    ),
+                    .init(
+                        color: Color(red: 0.76, green: 0.15, blue: 0.2),
+                        location: 0.3
+                    ),
+                ],
+                center: .top,
+                startRadius: 200,
+                endRadius: 700
+            )
             .ignoresSafeArea()
-            
+
             VStack {
                 Spacer()
-                
+
                 Text("Guess the Flag")
                     .font(.largeTitle.bold())
                     .foregroundStyle(.white)
-                
+
                 VStack(spacing: 15) {
                     VStack {
                         Text("Tap the flag of")
@@ -41,7 +55,7 @@ struct ContentView: View {
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-                    
+
                     ForEach(0..<3) { number in
                         Button {
                             flagTapped(number)
@@ -56,35 +70,35 @@ struct ContentView: View {
                 .padding(.vertical, 20)
                 .background(.regularMaterial)
                 .clipShape(.rect(cornerRadius: 20))
-                
+
                 Spacer()
                 Spacer()
-                
+
                 Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
-                
+
                 Spacer()
             }
             .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
-            } message: {
+        } message: {
             Text(scoreMessage)
         }
-            .alert("Game Over!", isPresented: $showingFinalScore) {
-                Button("Restart", action: resetGame)
-                } message: {
-                Text("Final Score: \(score) out of 8!")
-            }
-        
+        .alert("Game Over!", isPresented: $showingFinalScore) {
+            Button("Restart", action: resetGame)
+        } message: {
+            Text("Final Score: \(score) out of 8!")
+        }
+
     }
-    
+
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
-            score+=1
+            score += 1
             scoreMessage = "Your score is \(score)"
         } else {
             scoreTitle = "Wrong"
@@ -92,16 +106,17 @@ struct ContentView: View {
         }
         questionNumber += 1
         if questionNumber < 8 {
-            showingScore = true} else {
-                showingFinalScore = true
-            }
+            showingScore = true
+        } else {
+            showingFinalScore = true
+        }
     }
-    
+
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
-    
+
     func resetGame() {
         score = 0
         questionNumber = 0
