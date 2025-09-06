98c2330066d14eacca9e855c1097b8c734880a28
Daniel-Lidman <dlidman@gmail.com>
2025-09-06 18:15:30 -0500

    Day 27

---
+++ SwiftUI-project folders/project4/BetterRest_Daniel/Better Rest Daniel/Better Rest Daniel/Better_Rest_DanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Rest_DanielApp.swift
+//  Better Rest Daniel
+//
+//  Created by Daniel Lidman on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Better_Rest_DanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-project folders/project4/BetterRest_Daniel/Better Rest Daniel/Better Rest DanielTests/Better_Rest_DanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Rest_DanielTests.swift
+//  Better Rest DanielTests
+//
+//  Created by Daniel Lidman on 9/6/25.
+//
+
+import Testing
+@testable import Better_Rest_Daniel
+
+struct Better_Rest_DanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
