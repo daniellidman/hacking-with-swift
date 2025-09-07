10593cc582652d2e6f4fea05b6e489a66ec3391b
Daniel-Lidman <dlidman@gmail.com>
2025-09-07 12:14:10 -0500

    Day 29

---
+++ SwiftUI-project folders/project5/WordScramble Daniel/WordScramble Daniel/WordScramble Daniel/WordScramble_DanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_DanielApp.swift
+//  WordScramble Daniel
+//
+//  Created by Daniel Lidman on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_DanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-project folders/project5/WordScramble Daniel/WordScramble Daniel/WordScramble DanielTests/WordScramble_DanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_DanielTests.swift
+//  WordScramble DanielTests
+//
+//  Created by Daniel Lidman on 9/7/25.
+//
+
+import Testing
+@testable import WordScramble_Daniel
+
+struct WordScramble_DanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
