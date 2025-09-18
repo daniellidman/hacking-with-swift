1e64f001aa0620638b97659f6020486e077a9ef2
Daniel-Lidman <dlidman@gmail.com>
2025-09-17 20:53:41 -0500

    Day 38

---
+++ SwiftUI-project folders/project8/Moonshot Daniel/Moonshot Daniel/Moonshot_DanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_DanielApp.swift
+//  Moonshot Daniel
+//
+//  Created by Daniel Lidman on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_DanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-project folders/project8/Moonshot Daniel/Moonshot DanielTests/Moonshot_DanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_DanielTests.swift
+//  Moonshot DanielTests
+//
+//  Created by Daniel Lidman on 9/17/25.
+//
+
+import Testing
+@testable import Moonshot_Daniel
+
+struct Moonshot_DanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
