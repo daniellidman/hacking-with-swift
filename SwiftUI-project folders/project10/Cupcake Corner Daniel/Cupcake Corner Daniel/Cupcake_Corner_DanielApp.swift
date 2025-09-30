a6e0a0dbef51cbadf5599ec1b4518a45725b7ab0
Daniel-Lidman <dlidman@gmail.com>
2025-09-30 12:23:49 -0500

    Day 49

---
+++ SwiftUI-project folders/project10/Cupcake Corner Daniel/Cupcake Corner Daniel/Cupcake_Corner_DanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_Corner_DanielApp.swift
+//  Cupcake Corner Daniel
+//
+//  Created by Daniel Lidman on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_Corner_DanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-project folders/project10/Cupcake Corner Daniel/Cupcake Corner DanielTests/Cupcake_Corner_DanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_Corner_DanielTests.swift
+//  Cupcake Corner DanielTests
+//
+//  Created by Daniel Lidman on 9/30/25.
+//
+
+import Testing
+@testable import Cupcake_Corner_Daniel
+
+struct Cupcake_Corner_DanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
