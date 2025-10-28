12c2ae9016d1a7051997f8aa5ca33ce7cbc097a9
Daniel-Lidman <dlidman@gmail.com>
2025-10-28 12:34:21 -0500

    Day 62

---
+++ SwiftUI-project folders/project13/Instafilter Daniel/Instafilter Daniel/Instafilter_DanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter_DanielApp.swift
+//  Instafilter Daniel
+//
+//  Created by Daniel Lidman on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Instafilter_DanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-project folders/project13/Instafilter Daniel/Instafilter DanielTests/Instafilter_DanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter_DanielTests.swift
+//  Instafilter DanielTests
+//
+//  Created by Daniel Lidman on 10/28/25.
+//
+
+import Testing
+@testable import Instafilter_Daniel
+
+struct Instafilter_DanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
