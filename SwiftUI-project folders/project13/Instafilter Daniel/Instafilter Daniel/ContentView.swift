4506e5f10ea59ebfeb71a9b2513cca11c7195d83
Daniel-Lidman <dlidman@gmail.com>
2025-10-29 13:02:25 -0500

    Day 63

--- SwiftUI-project folders/project13/Instafilter Daniel/Instafilter Daniel/ContentView.swift
+++ SwiftUI-project folders/project13/Instafilter Daniel/Instafilter Daniel/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by Daniel Lidman on 10/28/25.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        let inputImage = UIImage(resource: .poetry) //
+        let beginImage = CIImage(image: inputImage)
+
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        // convert that to a UIImage
+        let uiImage = UIImage(cgImage: cgImage)
+
+        // and convert that to a SwiftUI image
+        image = Image(uiImage: uiImage)
     }
 }
 
