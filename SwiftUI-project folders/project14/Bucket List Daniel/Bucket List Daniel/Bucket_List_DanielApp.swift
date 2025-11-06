4981e0f0860d9e8955878b0bfce6174404dc15fb
Daniel-Lidman <dlidman@gmail.com>
2025-11-06 12:48:02 -0600

    Day 68a

---
+++ SwiftUI-project folders/project14/Bucket List Daniel/Bucket List Daniel/Bucket_List_DanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_List_DanielApp.swift
+//  Bucket List Daniel
+//
+//  Created by Daniel Lidman on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_List_DanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-project folders/project14/Bucket List Daniel/Bucket List DanielTests/Bucket_List_DanielTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_List_DanielTests.swift
+//  Bucket List DanielTests
+//
+//  Created by Daniel Lidman on 11/6/25.
+//
+
+import Testing
+@testable import Bucket_List_Daniel
+
+struct Bucket_List_DanielTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
